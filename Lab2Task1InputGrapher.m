076fc5173b5789a01ac76c3528b90cbfc6530235
Rafael Mohlin <raffe_mohlin@hotmail.com>
2016-10-25 17:01:49 +0200

    Lab2 Task1 plots in same figure

--- Lab2Task1InputGrapher.m
+++ Lab2Task1InputGrapher.m
@@ -3,32 +3,29 @@ input = csvread(sprintf(filePattern, 'input', sigma));
 ordering = csvread(sprintf(filePattern, 'ordering', sigma));
 convergance = csvread(sprintf(filePattern, 'convergance', sigma));
 
-[path, name, ~] = fileparts(sprintf(filePattern, 'ordering', sigma));
+[path, name, ~] = fileparts(sprintf(filePattern, 'result', sigma));
 outfile = sprintf('%s.png', name);
 outname = outfile;
 if path ~= ''
     outfile = strcat([path, filesep, outfile]);
 end
 
-h = figure('Name', outname, 'NumberTitle', 'off');
+h = figure('Name', outname, 'NumberTitle', 'off', 'Position', [100, 100, 1200, 600]);
+h.PaperPositionMode = 'auto';
+
+sp = subplot(1, 2, 1);
+set(sp,'LooseInset',get(sp,'TightInset'));
 hold on;
 scatter(input(:, 1), input(:, 2), '.blue');
-plot(ordering(:, 1), ordering(:, 2));
+plot(ordering(:, 1), ordering(:, 2), 'LineWidth', 1.5);
 plot([0 0.5 1 0], [0 1 0 0], 'black');
 title(sprintf('Weights after ordering, \\sigma_0 = %d', sigma));
-saveas(h, outfile);
 
-[path, name, ~] = fileparts(sprintf(filePattern, 'convergance', sigma));
-outfile = sprintf('%s.png', name);
-outname = outfile;
-if path ~= ''
-    outfile = strcat([path, filesep, outfile]);
-end
-
-h = figure('Name', outname, 'NumberTitle', 'off');
+sp = subplot(1, 2, 2);
+set(sp,'LooseInset',get(sp,'TightInset'));
 hold on;
 scatter(input(:, 1), input(:, 2), '.blue');
-plot(convergance(:, 1), convergance(:, 2));
+plot(convergance(:, 1), convergance(:, 2), 'LineWidth', 1.5);
 plot([0 0.5 1 0], [0 1 0 0], 'black'); 
 title(sprintf('Weights after convergance, \\sigma_0 = %d', sigma));
 saveas(h, outfile);
